bf1848c98f60343cf86f1ddef696d4a345910e4b
SolAZDev <SolAZDev@gmail.com>
2022-12-09 18:52:17 -0400

    Initial: First Layout Version

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+q-layout(view='hHh lpR fFf')
+  q-header.bg-dark.text-white.row(elevated, bordered)
+    q-toolbar.q-gutter-sm.col
+      q-btn(flat, round, dense, icon='home' size="lg")
+      q-btn(flat, dense, no-caps, label="Random")
+      q-btn(flat, dense, no-caps, label="Genre")
+    q-toolbar.col.row.justify-end.q-gutter-x-xs
+      q-icon.col-1(name='search' size="md")
+      q-input.col-10(v-model='searchQuery', type='text', label='Search' dark)
+
+  q-page-container
+    router-view
+
+  q-footer.bg-dark.text-white(reveal, elevated, bordered)
+    q-toolbar.justify-around
+      q-btn(flat, dense, no-caps, label="Submit a Comic")
+      q-btn(flat, dense, no-caps, label="Contact")
+      q-btn(flat, dense, no-caps, label="FAQ")
+</template>
+<script lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+q-page.fit.column.justify-evenly()
+  div.empty
+  .row.ComicsSection.fit
+    .col-12.text-h4 My Comics
+    .col-1.column.justify-center(@click='CheckComicPage(-1)') #[q-icon(name='chevron_left' size="md")]
+    .col.q-ma-md
+      .row.fit
+        .col-4.q-pa-sm(v-for="item in [,,,,,,,,,]")
+          q-card(square)
+            q-img(src='https://placeimg.com/300/300/nature', :ratio='1/1', spinner-color='primary', spinner-size='82px', fit="cover")
+    .col-1.column.justify-center(@click='CheckComicPage(-1)')  #[q-icon(name='chevron_right' size="md")]
+  div.empty
+  .row.SponsorSection.fit
+    .col-12.text-h4 Sponsors
+    .col-1.column.justify-center(@click='CheckComicPage(-1)') #[q-icon(name='chevron_left' size="md")]
+    .col.q-ma-md
+      .row.fit
+        .col-4.q-pa-sm(v-for="item in [,,,]")
+          q-card(square)
+            q-img(src='https://placeimg.com/300/300/tech', :ratio='1/1', spinner-color='primary', spinner-size='82px', fit="cover")
+    .col-1.column.justify-center(@click='CheckComicPage(-1)')  #[q-icon(name='chevron_right' size="md")]
+</template>
+<script setup lang="ts">
+
+</script>
